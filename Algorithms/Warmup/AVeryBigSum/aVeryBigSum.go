abe1d602c46dec673981e3ce81152cc03d95fee9
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-10-18 02:22:31 +0100

    feat: completed function aVeryBigSum

---
+++ Algorithms/Warmup/AVeryBigSum/aVeryBigSum.go
@@ -0,0 +1,115 @@
+/*
+	In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+
+# Function Description
+
+Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+
+aVeryBigSum has the following parameter(s):
+
+int ar[n]: an array of integers .
+Return
+
+long: the sum of all array elements
+Input Format
+
+The first line of the input consists of an integer .
+The next line contains  space-separated integers contained in the array.
+
+# Output Format
+
+Return the integer sum of the elements in the array.
+
+# Constraints
+1 <= n <= 10
+0 <= ar[i] <= 10**10
+
+# Sample Input
+
+5
+1000000001 1000000002 1000000003 1000000004 1000000005
+Output
+
+5000000015
+Note:
+
+The range of the 32-bit integer is .
+When we add several integer values, the resulting sum might exceed the above range.
+You might need to use long int C/C++/Java to store such sums.
+*/
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'aVeryBigSum' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+ */
+
+func aVeryBigSum(ar []int64) int64 {
+	n := len(ar)
+	var sum int64 = 0
+	if 1 <= n || n <= 10 {
+		for i := 0; i < n; i++ {
+			if 0 <= ar[i] || int64(math.Pow(10, 10)) <= ar[i] {
+				sum += ar[i]
+			}
+		}
+	}
+	return sum
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	arCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+
+	arTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var ar []int64
+
+	for i := 0; i < int(arCount); i++ {
+		arItem, err := strconv.ParseInt(arTemp[i], 10, 64)
+		checkError(err)
+		ar = append(ar, arItem)
+	}
+
+	result := aVeryBigSum(ar)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
